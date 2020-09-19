dd68fd7ef463648dcf22cbdb0466140051808c16
danicelistobon <danielct-97@hotmail.com>
2020-09-19 12:28:16 -0500

    Created footer component

---
+++ src/components/Common/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <hr />
+      <div className='text-center my-4 text-muted'>
+        &copy; Daniel Celis Tobon - 2020
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
